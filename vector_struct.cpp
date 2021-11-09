1eefaf847ef611702f9830f30d2b4ef32f75570f
sd1219 <sd1219@ic.ac.uk>
2021-11-09 16:18:39 +0000

    Vector

--- vector_struct.cpp
+++ vector_struct.cpp
@@ -5,6 +5,7 @@ using namespace std;
 struct my_int_vector{
     int *elements;
     int count;
+    int capacity;
     };
 
 int size(my_int_vector *v){
@@ -32,7 +33,7 @@ my_int_vector iota(int n){
 }
 
 void destroy(my_int_vector *v){
-    delete []v->elements
+    delete []v->elements;
     delete []v;
 }
 
@@ -43,6 +44,40 @@ my_int_vector *create(int size){
     return res;
 }
 
+void print(my_int_vector *vec){
+
+    cout<<"<"<<vec->elements[0];
+    for(int i = 1; i< vec->count; i++){
+        cout<<", "<<vec->elements[i];
+    }
+    cout<<">"<<endl;
+}
+
+void resize(my_int_vector *vec, int new_size){
+    my_int_vector *new_vec = create(new_size);
+    for(int i = 0; i < vec->count; i++){
+        new_vec->elements[i] = vec->elements[i];
+    }
+    for (int i = vec->count; i < new_size; i++){
+        new_vec->elements[i] = 0;
+    }
+    vec->elements = new_vec->elements;
+    vec-> count = new_vec->count;
+    delete []new_vec;
+}
+
+void push_back(my_int_vector *vec, int v){
+    
+}
+
+
+
 int main(){
-   return 0;
+    my_int_vector *vec = create(10);
+    *vec = iota(5);
+    print(vec);
+    resize(vec, 3);
+    print(vec);
+    
+    return 0;
 }
